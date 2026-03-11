7bf3003b681c7bb83a3daf7e76f743132c3bcb23
Mikhail <041411032425mk@gmail.com>
2026-03-11 22:57:46 +0400

    feat: add header component and update layout feat: add heroui theme and tailwindcss fix: update next.config.ts for turbopack compatibility

--- src/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-const About = () => {
-    return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"> 
-        <h1 className="text-4xl font-bold">About Us</h1>
-    </main>
-    );
-};
-
-export default About;
